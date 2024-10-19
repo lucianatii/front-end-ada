faff148c181d7ae4008071c7b80b3373945a676b
Luciana Silva <142263885+lucianatii@users.noreply.github.com>
2024-10-19 18:48:27 -0300

    Criando componente Header

--- 08 - React I/my-app/src/App.jsx
+++ 08 - React I/my-app/src/App.jsx
@@ -1,22 +1,15 @@
 import React from "react";
+import "./assets/styles/App.css";
+import Header from "./components/Header/Header";
 
 //Componentes em classes são classes que herdam a classe Component do React e retornam HTML dentro do método render
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.nome = "Luciana";
-  }
-
   //render é um método responsável por renderizar todo o conteudo HTML do nosso componente
   render() {
     return (
-      <div>
-        <h1>First Project</h1>
-        <h2>{this.nome}</h2>
-        <p>Primeiro páragrafo</p>
-      </div>
-    );
+    <Header />
+    )
   }
 }
 
---
+++ 08 - React I/my-app/src/components/Header/Header.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import brand from "../../assets/img/brand.png";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <nav id="navBar">
+          <div className="navBrand">
+            <img src={brand} alt="" />
+            <h1>Space Flight News</h1>
+          </div>
+
+          <ul className="navList">
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/">Trending</a>
+            </li>
+            <li>
+              <a href="/">Categories</a>
+            </li>
+            <li>
+              <a href="/">About Us</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
+}
+
+export default Header;
