7d755f8661715ba876d5be6cd5fd26e93302473b
Luciana Silva <142263885+lucianatii@users.noreply.github.com>
2024-10-19 16:47:47 -0300

    Conceito de Componentes em Classes

--- 08 - React I/my-app/src/App.js
+++ 08 - React I/my-app/src/App.js
@@ -1,9 +1,23 @@
-function App() {
-  return (
-    <div>
-      <h1>First Project</h1>
-    </div>
-  );
+import React from "react";
+
+//Componentes em classes são classes que herdam a classe Component do React e retornam HTML dentro do método render
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.nome = "Luciana";
+  }
+
+  //render é um método responsável por renderizar todo o conteudo HTML do nosso componente
+  render() {
+    return (
+      <div>
+        <h1>First Project</h1>
+        <h2>{this.nome}</h2>
+        <p>Primeiro páragrafo</p>
+      </div>
+    );
+  }
 }
 
 export default App;
