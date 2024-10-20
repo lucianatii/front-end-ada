3be056fe1e6c324538008f9ee7993bc15abddc8f
Luciana Silva <142263885+lucianatii@users.noreply.github.com>
2024-10-20 15:55:20 -0300

    Pegando valores digitados no Form com setState

--- 08 - React I/contador/src/components/Contador/Contador.jsx
+++ 08 - React I/contador/src/components/Contador/Contador.jsx
@@ -1,27 +1,61 @@
 import React from "react";
-import "../Contador/styles.css"
+import "../Contador/styles.css";
 
 export class Contador extends React.Component {
-constructor(){
-  super()
-  // this.contador = 0;
-  this.state = {contador: 0}
-}
+  constructor() {
+    super();
+    // this.contador = 0;
+    this.state = { contador: 0, name: "", password: "" };
+  }
 
-  render (){
+  render() {
     return (
-      <div className="container">
-        
-        <h1>time is passing...</h1>
-        <h2>{this.state.contador}</h2>
+      <div>
+        <div className="containerCounter">
+          <h1>time is passing...</h1>
+          <h2>{this.state.contador}</h2>
 
-        <div className="botoes">
-          <button onClick={() => {this.setState({contador: this.state.contador + 1})}}>Aumentar</button>
-          <button onClick={() => {this.setState({contador: this.state.contador - 1})}}>Diminuir</button>  
-        </div>        
-     
+          <div className="botoes">
+            <button
+              onClick={() => {
+                this.setState({ contador: this.state.contador + 1 });
+              }}
+            >
+              Aumentar
+            </button>
+            <button
+              onClick={() => {
+                this.setState({ contador: this.state.contador - 1 });
+              }}
+            >
+              Diminuir
+            </button>
+          </div>
+        </div>
+
+        <div className="containerForm">
+          <h2>pegando valores digitados...</h2>
+          <form>
+            <input
+              type="text"
+              placeholder="Name"
+              value={this.state.name}
+              onChange={(event) => {
+                this.setState({ name: event.target.value });
+              }}
+            />
+            <input
+              type="text"
+              placeholder="Password"
+              value={this.state.password}
+              onChange={(event) => {
+                this.setState({ password: event.target.value });
+              }}
+            />
+            <button>Send</button>
+          </form>
+        </div>
       </div>
-    )
+    );
   }
 }
-
