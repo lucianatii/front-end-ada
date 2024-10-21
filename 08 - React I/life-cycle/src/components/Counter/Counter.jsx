3563eb906e24d3dff49ce5f6e50013e9d127e147
Luciana Silva <142263885+lucianatii@users.noreply.github.com>
2024-10-21 17:35:25 -0300

    Conhecendo componentes de Renderização - componentWillMount e DidMount

--- 08 - React I/life-cycle/src/components/Counter/Counter.jsx
+++ 08 - React I/life-cycle/src/components/Counter/Counter.jsx
@@ -1,13 +1,38 @@
 import React from "react";
 
 class Counter extends React.Component {
+  constructor() {
+    super();
+    this.state = { contador: 0 };
+    console.log("Construindo a Classe");
+  }
+
+  UNSAFE_componentWillMount() {
+    console.log("Componente contador será montado");
+  }
+
+  componentDidMount() {
+    console.log("Componente ja foi montado, ok?");
+  }
+
+
+
   render() {
+    console.log("Renderizando componente")
     return (
       <div>
-        <h2>0</h2>
+        <h2>{this.state.contador}</h2>        
 
-        <button>Aumentar</button>
-        <button>Diminuir</button>
+        <button
+          onClick={() => {
+            this.setState({ contador: this.state.contador + 1 });
+          }}
+        >
+          Aumentar
+        </button>
+        <button onClick={() => {
+            this.setState({ contador: this.state.contador - 1 });
+          }}>Diminuir</button>
       </div>
     );
   }
