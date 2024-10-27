f3cc6f35113f42aa61adaa6f4f64993d261baafc
Luciana Silva <142263885+lucianatii@users.noreply.github.com>
2024-10-27 13:08:38 -0300

    Conceito de useEffect

--- 08 - React I/to-do-list/src/App.tsx
+++ 08 - React I/to-do-list/src/App.tsx
@@ -1,14 +1,27 @@
-import React from "react";
-
+import { useEffect, useState } from "react";
 import { Header } from "./Components/Header/Header";
 import "./Styles/global.css";
 import { Tasks } from "./Components/Tasks/Tasks";
 
 function App() {
+  const [toggle, setToggle] = useState(false);
+  useEffect(() => {
+    console.log("executando a funcao use effect");
+
+    //componentWillUnmount = CleanUp Function
+    return () => {
+      console.log("Executado quando o componente for desmontado");
+    };
+  }, [toggle]);
+
+  //useEffect será disparado sempre que uma variável do array for alterada
+  //Por padrão será disparado sempre após a montagem de um componente (equivalente a componentDidMount)
   return (
     <div>
       <Header />
       <Tasks />
+
+      <button onClick={() => setToggle(!toggle)}>Toggle</button>
     </div>
   );
 }
