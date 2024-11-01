929adcb6a36c826b2314402433f1d7e734154aac
Luciana Silva <142263885+lucianatii@users.noreply.github.com>
2024-11-01 14:39:24 -0300

    Refatorando App e Instalando Yarn

--- 08 - React I/my-app/src/App.jsx
+++ 08 - React I/my-app/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./assets/styles/App.css";
 import { Header } from "./components/Header/Header";
 import { Article } from "./components/Article/Article";
@@ -6,36 +6,54 @@ import article1 from "./assets/img/article1.png";
 import article2 from "./assets/img/article2.png";
 import article3 from "./assets/img/article3.png";
 
-
 //Componentes em classes são classes que herdam a classe Component do React e retornam HTML dentro do método render
 //Componentes funcionais são funcões que retornam HTML
 
-class App extends React.Component {
+function App() {
+  const [news, setNews] = useState();
+
+  useEffect(() => {
+    fetch();
+  }, []);
+
   //render é um método responsável por renderizar todo o conteudo HTML do nosso componente
-  render() {
-    return (
-      //tag genérica fragment <></>
-      <>
-        <Header/>
 
-        <section id="articleList">
-        <Article title="Designing Dashboards" provider="NASA" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum non, quos magni odio distinctio ipsam sequi consequuntur quo aspernatur at obcaecati eveniet quidem maxime vero beatae ad est atque quia!"
-        thumbnail={article1}/>
+  return (
+    //tag genérica fragment <></>
+    <>
+      <Header />
 
-        <Article title="Vibrant Portraits of 2020" provider="SpaceNews" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum non, quos magni odio distinctio ipsam sequi consequuntur quo aspernatur at obcaecati eveniet quidem maxime vero beatae ad est atque quia!"
-        thumbnail={article2}/>
+      <section id="articleList">
+        <Article
+          title="Designing Dashboards"
+          provider="NASA"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum non, quos magni odio distinctio ipsam sequi consequuntur quo aspernatur at obcaecati eveniet quidem maxime vero beatae ad est atque quia!"
+          thumbnail={article1}
+        />
 
-        <Article title="36 Days of Malayalam type" provider="SpaceFlight Now" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum non, quos magni odio distinctio ipsam sequi consequuntur quo aspernatur at obcaecati eveniet quidem maxime vero beatae ad est atque quia!"
-        thumbnail={article3}/>
+        <Article
+          title="Vibrant Portraits of 2020"
+          provider="SpaceNews"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum non, quos magni odio distinctio ipsam sequi consequuntur quo aspernatur at obcaecati eveniet quidem maxime vero beatae ad est atque quia!"
+          thumbnail={article2}
+        />
 
-        <Article title="Designing Dashboards" provider="NASA" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum non, quos magni odio distinctio ipsam sequi consequuntur quo aspernatur at obcaecati eveniet quidem maxime vero beatae ad est atque quia!"
-        thumbnail={article1}/>
-        </section>
-        
-      </>
-    
-    )
-  }
+        <Article
+          title="36 Days of Malayalam type"
+          provider="SpaceFlight Now"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum non, quos magni odio distinctio ipsam sequi consequuntur quo aspernatur at obcaecati eveniet quidem maxime vero beatae ad est atque quia!"
+          thumbnail={article3}
+        />
+
+        <Article
+          title="Designing Dashboards"
+          provider="NASA"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum non, quos magni odio distinctio ipsam sequi consequuntur quo aspernatur at obcaecati eveniet quidem maxime vero beatae ad est atque quia!"
+          thumbnail={article1}
+        />
+      </section>
+    </>
+  );
 }
 
 export default App;
--- 08 - React I/my-app/src/components/Header/Header.jsx
+++ 08 - React I/my-app/src/components/Header/Header.jsx
@@ -1,36 +1,36 @@
 import React from "react";
 import brand from "../../assets/img/brand.png";
-import "../Header/styles.css"
+import "../Header/styles.css";
 
 // export class Header extends React.Component
-export function Header () {
+export function Header() {
   // render() {
-    return (
-      <header>
-        <nav id="navBar">
-          <div className="navBrand">
-            <img src={brand} alt="" />
-            <h1>Space Flight News</h1>
-          </div>
+  return (
+    <header>
+      <nav id="navBar">
+        <div className="navBrand">
+          <img src={brand} alt="" />
+          <h1>Space Flight News</h1>
+        </div>
 
-          <ul className="navList">
-            <li>
-              <a href="/">Home</a>
-            </li>
-            <li>
-              <a href="/">Trending</a>
-            </li>
-            <li>
-              <a href="/">Categories</a>
-            </li>
-            <li>
-              <a href="/">About Us</a>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    );
-  }
+        <ul className="navList">
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">Trending</a>
+          </li>
+          <li>
+            <a href="/">Categories</a>
+          </li>
+          <li>
+            <a href="/">About Us</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
 // }
 
 // export default Header;
