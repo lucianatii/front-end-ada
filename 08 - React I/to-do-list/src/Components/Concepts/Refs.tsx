1118b7f8b509ee75100000112fdc4386aba3f02a
Luciana Silva <142263885+lucianatii@users.noreply.github.com>
2024-11-02 17:53:21 -0300

    Focando em elementos HTML utilizando useRef

--- 08 - React I/to-do-list/src/Components/Concepts/Refs.tsx
+++ 08 - React I/to-do-list/src/Components/Concepts/Refs.tsx
@@ -1,23 +1,29 @@
-import { useRef, useState } from "react";
+import { InputHTMLAttributes, useEffect, useRef, useState } from "react";
 
 export const Refs: React.FC = () => {
-  const ref = useRef(0); //{current: 0}
-  const [toggle, setToggle] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null); //{current: 0}
+  // console.log(inputRef);
 
-  function handleClickOnButton() {
-    ref.current = ref.current + 1;
-    console.log(ref.current);
+  // useEffect(() => {
+  //   console.log(inputRef);
+  // }, [inputRef]);
+
+  function handleOnClick() {
+    if (inputRef.current) {
+      inputRef.current.focus(); //colocando o foco no input utilizando a referencia inputRef
+    }
   }
 
   return (
     <div>
       <h1>useRef</h1>
 
-      <p>{`O valor da Ref é ${ref.current}`}</p>
+      {/* <p>{`O valor da Ref é ${ref.current}`}</p> */}
       <br />
-      <button onClick={handleClickOnButton}>Clique Aqui</button>
+      <input type="text" placeholder="Nome Completo" ref={inputRef} />
+      <button onClick={handleOnClick}>Foque no Input</button>
       <br />
-      <button onClick={() => setToggle(!toggle)}>Toggle</button>
+      {/* <button onClick={() => setToggle(!toggle)}>Toggle</button> */}
 
       {/* refs nao disparam novas renderizações, por isso, seu valor continua estático na tela do usuário */}
     </div>
